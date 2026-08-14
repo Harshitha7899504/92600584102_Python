b82eeccbf87dd1140dcd4c6c6c8c061d960aac36
Harshitha7899504 <harshithamedisetti2005@gmail.com>
2026-08-14 20:36:05 +0530

    Delete ex-1.py

--- ex-1.py
+++
@@ -1,6 +0,0 @@
-#1. Write a program to demonstrate basic input and output operations in python.
-
-print("-----Basic Details of a Student-----")
-name=input("Enter your Name:")
-age=int(input("Enter your age:"))
-course=input("Enter Course:")
